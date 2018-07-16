80739548b6cd9e3d1d9b2f37f03e30f41ca3e20b
Travis H <tphdevdrop@gmail.com>
2018-07-16 14:38:28 -0500

    Update ex-01-sumMinZero.js

--- ex-01-sumMinZero.js
+++ ex-01-sumMinZero.js
@@ -1,12 +1,16 @@
 //------------------------------------------------------------------
-// sumMinZero()
-//   Write a function called sumMinZero that returns zero
-//   if the sum of two numbers is negative
+// TASK 1 : sumOrZero()
+//  Write a function called sumOrZero that returns zero
+//  if the sum of two numbers is negative
 //
 //------------------------------------------------------------------
 
-console.assert(sumMinZero(10, 10) === 20)
-console.assert(sumMinZero(30, 10) === 40)
-console.assert(sumMinZero(-30, 10) === 0)
-console.assert(sumMinZero(-9893, -8839) === 0)
-console.assert(sumMinZero(1848, -4124) === 0)
+console.log("====== sumOrZero ======")
+
+console.assert(sumOrZero(0, 5) === 5)
+console.assert(sumOrZero(10, 10) === 20)
+console.assert(sumOrZero(30, 10) === 40)
+
+console.assert(sumOrZero(-30, 10) === 0)
+console.assert(sumOrZero(-10, -10) === 0)
+console.assert(sumOrZero(-5, 0) === 0)
