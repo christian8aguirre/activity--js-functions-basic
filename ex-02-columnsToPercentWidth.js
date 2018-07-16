68289d449d501d12c9b2d0c2697c267abcb52743
Travis H <tphdevdrop@gmail.com>
2018-07-16 14:43:31 -0500

    Update and rename ex-02-getInput.js to ex-02-columnsToPercentWidth.js

---
+++ ex-02-columnsToPercentWidth.js
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------
+// TASK 2 : columnsToPercentWidth()
+//  Write a function called columnsToPercentWidth() that accepts the number of columns 
+//  as input and returns a string with the width percentage 
+//
+//  Examine the functionality of `columnsToPctWidth` in the function below 
+//.   Formula: ( 100 / «input» )
+//
+//
+//. Note: you may need to use the `parseInt()` utility to convert the decimal to an integer
+//    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+// 
+//------------------------------------------------------------------
+
+
+console.log("====== columnsToPercentWidth ======")
+
+console.assert(columnsToPercentWidth(1) === "100%")
+console.assert(columnsToPercentWidth(2) === "50%")
+console.assert(columnsToPercentWidth(3) === "33%")
+console.assert(columnsToPercentWidth(4) === "25%")
+console.assert(columnsToPercentWidth(5) === "20%")
+console.assert(columnsToPercentWidth(6) === "16%")
+console.assert(columnsToPercentWidth(10) === "10%")
+
+// Handle entry of 0 or negative numbers
+console.assert(columnsToPercentWidth(0) === "0%")
+console.assert(columnsToPercentWidth(-3) === "0%")
+
+
+
